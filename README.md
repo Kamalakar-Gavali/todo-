926fbe538ef27e6aac8db99bb2ea84a5fab64e1a
Kamalakar <kamalakar@nssplindia.com.com>
2021-11-05 01:51:43 +0530

    done

--- README.md
+++ README.md
@@ -1,70 +1,56 @@
-# Getting Started with Create React App
+<!-- Please update value in the {}  -->
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<h1 align="center">Windbnb</h1>
+<div align="center">
+  <h3>
+    <a href="https://pedantic-boyd-abd421.netlify.app/">
+      Demo
+    </a>
+  
+  </h3>
+</div>
 
-## Available Scripts
+<!-- TABLE OF CONTENTS -->
 
-In the project directory, you can run:
+## Table of Contents
 
-### `npm start`
+- [Overview](#overview)
+  - [Built With](#built-with)
+- [Features](#features)
+- [Contact](#contact)
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+<!-- OVERVIEW -->
 
-### `npm test`
+## Overview
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+![screenshot](./src/resources/todo.png)
 
-### `npm run build`
+<!-- Introduce your projects by taking a screenshot or a gif. Try to tell visitors a story about your project by answering:
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+- Where can I see your demo?
+- What was your experience?
+- What have you learned/improved?
+- Your wisdom? :) -->
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+### Built With
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+<!-- This section should list any major frameworks that you built your project using. Here are a few examples.-->
 
-### `npm run eject`
+- [React](https://reactjs.org/)
+- [styled-components](https://styled-components.com/)
+- [Material Icons](https://mui.com/components/material-icons/)
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+## Features
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+- Responsive
+- Add task
+- Delete task
+- delete all task
+- Categorization according to active or completed status
+- Persistent data Storage
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+## Contact
+- LinkedIn [Kamalakar Gavali](https://in.linkedin.com/in/kamalakar-gavali)
+- Twitter [@kamalakar_g1](https://twitter.com/kamalakar_g1)
---
+++ src/componets/Container.jsx
@@ -0,0 +1,334 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import DeleteIcon from '@mui/icons-material/DeleteOutline';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+const Wrapper = styled.div`
+width: 100%;
+max-width: 608px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+font-family:Montserrat;
+padding:10px;
+margin:0 auto;
+& hr{
+    margin:0px 0px 30px 0px;
+}
+
+`;
+
+
+const Heading = styled.h1`
+font-family: Raleway;
+font-style: normal;
+font-weight: bold;
+font-size: 36px;
+line-height: 42px;
+text-align: center;
+letter-spacing: -0.045em;
+
+
+color: #333333;
+`;
+const StatusStrip = styled.div`
+
+display:flex;
+justify-content:space-around;
+
+& .status{
+    display:flex;
+    flex-direction:column;
+    cursor:pointer;
+    & p{
+        font-weight: 600;
+    //   font-size: 14px;
+    font-family:inherit;
+    }
+    & div{
+    width: 89px;
+    height: 4px;
+
+    background: #2F80ED;
+    border-radius: 4px 4px 0px 0px;
+    &.show{
+        visibility:visible;
+    }
+    &.hide{
+        visibility:hidden;
+    }
+    }
+}
+`;
+const InputArea = styled.div`
+
+display:flex;
+justify-content:space-between;
+width:100%;
+// margin:30px 0px;
+&.show{
+    display:flex;  
+   }
+&.hide{
+       display:none
+ }
+@media screen and (max-width:600px)
+{
+    flex-direction:column;
+    height:140px;
+    justify-content:space-between;
+    align-items:center;
+}
+
+& div{
+
+    @media screen and (max-width:600px)
+{
+ width:100%
+}
+& input{
+padding:20px 12px;
+width:476px;
+
+border: 1px solid #BDBDBD;
+box-sizing: border-box;
+border-radius: 12px;
+color: #828282;
+font-size:17px;
+font-family:inherit;
+@media screen and (max-width:600px)
+{
+ width:100%
+}
+}
+
+
+&.err{
+    color: red;
+    text-align: left;
+    margin: 10px 5px;
+}
+}
+& button{
+color:white;
+    background: #2F80ED;
+box-shadow: 0px 2px 6px rgba(127, 177, 243, 0.4);
+border:1px solid #2F80ED;
+border-radius: 12px;
+width: 109px;
+
+height: 56px;
+font-weight: 600;
+font-size: 14px;
+cursor:pointer;
+}
+`;
+const ListArea = styled.div`
+
+text-align:left;
+ 
+
+& div{
+padding:20px 0px;
+font-family:Montserrat;
+font-size:18px;
+display:flex;
+align-items:center;
+&.completeList{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding:0px;
+    // margin-top:30px;
+    & span{
+        cursor:pointer;
+        color:#BDBDBD;
+    }
+}
+
+
+& input{
+width: 20px;
+height: 20px;
+border: 1px solid #828282;
+box-sizing: border-box;
+border-radius: 4px;
+cursor:pointer;
+
+}
+&>span{
+    margin-left:20px;
+    &.completed{
+        text-decoration: line-through;
+        }
+    &.active{
+        text-decoration: none;
+    }    
+}
+
+}
+& .deleteAll-btn{
+display:flex;
+justify-content:flex-end;
+button{
+    width: 124px;
+height: 40px;
+border:1px solid #EB5757;
+background: #EB5757;
+border-radius: 4px;
+color:white;
+display:flex;
+align-items:center;
+justify-content:space-around;
+cursor:pointer;
+
+}
+}
+
+`;
+
+const Footer = styled.footer`
+font-family: Montserrat;
+font-style: normal;
+font-weight: 500;
+font-size: 14px;
+text-align: center;
+color: #A9A9A9;
+display: flex;
+justify-content: center;
+align-items: center;
+margin-bottom: 10px;
+&>span{
+    color:tomato;
+    margin:0px 5px;
+}
+`;
+
+const Container = () => {
+
+
+    const [paneState, setPaneState] = useState([true, false, false])
+    const [taskList, setTaskList] = useState(JSON.parse(localStorage.getItem('taskList')) || []);
+    const [task, setTask] = useState('');
+    const [err, setErr] = useState(false);
+    let statusList = ['All', 'Active', 'Completed'];
+
+    const changePane = (index) => {
+        let tempState = [...new Array(paneState.length)];
+        tempState[index] = true;
+        setPaneState(tempState);
+
+    }
+    const addToTaskList = (task) => {
+
+        let temptaskList = taskList.slice();
+        if (task.trim() != '') {
+            temptaskList.push({ name: task, status: 'active' });
+            setTaskList(temptaskList);
+            setErr(false)
+            localStorage.setItem('taskList', JSON.stringify(temptaskList))
+        }
+        else {
+            setErr(true)
+        }
+        setTask('');
+    }
+
+    const changeStatus = (index) => {
+        console.log('old', taskList);
+        // debugger;
+        let temptaskList = taskList.slice();
+
+        if (temptaskList[index].status == 'active') {
+            temptaskList[index].status = 'completed';
+        }
+        else {
+            temptaskList[index].status = 'active';
+        }
+        console.log("new", temptaskList)
+        setTaskList(temptaskList);
+        localStorage.setItem('taskList', JSON.stringify(temptaskList))
+        // setTask('');
+    }
+
+    const deleteFromTaskList = (index) => {
+        let temptaskList = taskList.slice();
+        temptaskList.splice(index, 1);
+        // temptaskList.filter((task)=>)
+        setTaskList(temptaskList);
+        localStorage.setItem('taskList', JSON.stringify(temptaskList))
+    }
+    return (
+        <>
+            <Wrapper>
+                <Heading>#todo</Heading>
+                <StatusStrip>
+                    {
+                        statusList.map((text, index) =>
+                            <div className='status' onClick={() => changePane(index)}>
+                                <p>{text}</p>
+                                <div className={paneState[index] ? 'show' : 'hide'}></div>
+
+                            </div>
+                            // <button className={ paneState[index]&& 'border-bottom'} onClick={()=>changePane(index)}>{text}</button>
+                        )
+                    }
+
+                </StatusStrip>
+                <hr></hr>
+
+                <InputArea className={(paneState[0] || paneState[1]) ? 'show' : 'hide'}>
+
+                    <div>
+                        <input placeholder='add details' value={task} onChange={(e) => setTask(e.target.value)} onKeyDown={(e) => e.keyCode == 13 && addToTaskList(task)} />
+                        {err &&
+                            <div className='err'>Enter task</div>
+                        }
+                    </div>
+
+                    <button onClick={() => addToTaskList(task)}>Add</button>
+
+                </InputArea>
+                <ListArea>
+                    {
+                        (taskList.length > 0) && taskList.map((task, index) => <>
+
+
+                            {(paneState[0] || (paneState[1] && task.status == 'active')) && (
+                                <div key={index} >
+                                    <input type='checkbox' checked={task.status == 'completed' ? true : false} onClick={() => changeStatus(index)} />
+                                    <span className={task.status == 'completed' ? 'completed' : 'active'}>{task.name}</span>
+                                </div>
+                            )
+                            }
+                            {
+                                paneState[2] && task.status == 'completed' && (<>
+                                    <div className='completeList'>
+                                        <div key={index} >
+                                            <input type='checkbox' checked={task.status == 'completed' ? true : false} onClick={() => changeStatus(index)} />
+                                            <span className={task.status == 'completed' ? 'completed' : 'active'}>{task.name}</span>
+                                        </div>
+                                        <span><DeleteIcon onClick={() => deleteFromTaskList(index)} /></span>
+                                    </div>
+
+                                </>
+                                )
+                            }
+                        </>
+                        )
+                    }
+                    {paneState[2] && (taskList.length > 0) && (taskList.filter(task => task.status == 'completed').length > 0) && (
+                        <div className='deleteAll-btn'>
+                            <button onClick={() => { setTaskList([]); localStorage.setItem('taskList', []) }}><span><DeleteIcon /></span> Delete All</button>
+                        </div>
+                    )
+                    }
+                </ListArea>
+
+            </Wrapper>
+            <Footer>Created with <span><FavoriteIcon /></span> by Kamalakar Gavali</Footer>
+        </>
+    )
+
+}
+
+export default Container;
